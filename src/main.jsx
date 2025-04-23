e15cd04af20b262984c835e534801b932edcbc43
emamex98 <emamex98@gmail.com>
2025-04-23 14:09:08 -0600

    Add Sentry

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from '@context/auth-context';
 import App from './App';
+import * as Sentry from '@sentry/react';
 
 import './index.css';
 
+Sentry.init({
+  dsn: 'https://e78ab80a93b20db644d031b04e658854@o4509201126981632.ingest.us.sentry.io/4509204378288128',
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
   <BrowserRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
   </BrowserRouter>
-  // </React.StrictMode>
 );
